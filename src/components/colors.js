f12d92e675b00e32c8da92ae0bfcdf2e3cc72e1d
Nelson <jhoeladarmes@gmail.com>
2025-04-19 13:35:26 -0300

    agregué mas colores

--- src/components/colors.js
+++ src/components/colors.js
@@ -1,20 +1,27 @@
 export const colors = [
+  "super-red", 
   "red",        // Reds
   "orange",     
   "gold",       
   "yellow",     
   "lime",       // Greens
-  "green",      
-  "olive",      
+  "green",
+  "soft-green",      
+  "olive",
+  "brown",    
   "teal",       // Teal/Cyan
-  "cyan",       
-  "blue",       // Blues
+  "cyan",
+  "soft-blue",   // Blues      
+  "blue",     
   "navy",       
-  "indigo",     
+  "indigo",
+  "bordo",     
   "purple",     
   "violet",     
   "magenta",    // Pinks/Purples
-  "pink",       
+  "pink",
+  "soft-pink",
+  "beige",       
   "silver",     // Grays/Neutrals
   "gray",       
   "white",      
